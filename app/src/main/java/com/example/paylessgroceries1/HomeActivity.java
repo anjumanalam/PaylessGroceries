eab5636786aee402bb664a4eddb09e04a522c0be
anjumanalam <aaalam@syr.edu>
2020-03-14 16:51:06 -0400

    Add button to HomeActivity layout that will be used to allow user to add an item to their shopping list.

--- app/src/main/java/com/example/paylessgroceries1/HomeActivity.java
+++ app/src/main/java/com/example/paylessgroceries1/HomeActivity.java
@@ -5,42 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btAddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
-//    //Menu button on Toolbar
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu,menu);
-//        return true;
-//    }
-//
-//    //Options for Menu button on toolbar
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        int id = item.getItemId();
-//
-//        if (id == R.id.share){
-//
-//        } else if (id == R.id.about) {
-//
-//        } else if (id == R.id.logout) {
-//
-//        } else if (id == R.id.search) {
-//
-//        } else if (id == R.id.settings) {
-//
-//        }
-//        return true;
-//    }
+    //Menu button on Toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    //Options for Menu button on toolbar
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.share){
+
+        } else if (id == R.id.about) {
+
+        } else if (id == R.id.logout) {
+
+        } else if (id == R.id.search) {
+
+        } else if (id == R.id.settings) {
+
+        }
+        return true;
+    }
 }
